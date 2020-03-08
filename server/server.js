d5866995864a31001d56c50431963aa8c7f20795
Ricardo <ricardo@ricardofideles.com.br>
2020-03-08 16:59:55 -0300

    Ajustando basePath.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('baseUrl ==>', baseUrl)
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
